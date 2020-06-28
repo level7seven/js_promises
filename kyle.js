dd4ecf938d4ad19b4cf3e4c867dc3a5c6dcce3d8
level7seven <level7seven@outlook.com>
2020-06-28 17:48:19 +0200

    Promises from wbdev simplified

---
+++ kyle.js
@@ -0,0 +1,23 @@
+// promises 
+
+let p = new Promise((resolve, reject) => {
+    let a = 1+2
+    if (a == 2) {
+        resolve('Success')
+    } else {
+        reject('Failed')
+    }
+})
+
+p.then((message) => {
+    console.log('this msg is in the then ' + message)
+
+}).catch((message) => {
+    console.log('this is in the catch') + message
+})
+
+console.log(p)
+
+let w = new Promise((resolve, rreject) => {
+
+})
